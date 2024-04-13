b1dd13afa62f440a5588231fb48218b89f0d6c3d
GegerOk <166231334+GegerOk@users.noreply.github.com>
2024-04-13 15:19:46 +0300

    Add files via upload

---
+++ hm5.py
@@ -0,0 +1,13 @@
+my_list = ['banana', 'pineaple', 'peach', 'apple', 'cherry']
+print (my_list)
+print(my_list [0])
+print(my_list [-1])
+print(my_list [2:5])
+my_list [2] = 'hleb'
+print (my_list)
+
+my_dict = {'banana': 'банан', 'pineaple': 'ананас', 'peach': 'персик'}
+print (my_dict)
+print ('Перевод ', my_dict ['peach'])
+my_dict.update ({'bread': 'xлеб'})
+print (my_dict)
