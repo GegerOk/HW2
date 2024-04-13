ae17d4935b17e687c06ebc0204e1ad095a20f8a1
GegerOk <166231334+GegerOk@users.noreply.github.com>
2024-04-13 14:37:43 +0300

    Add files via upload

---
+++ hw4.py
@@ -0,0 +1,6 @@
+immutable_var = 1, 2, 3, 4, True, 'Hello'
+print (immutable_var)
+#immutable_var [0] = 4  Неизменяемый кортеж
+mutable_list = [1, 2, 3, 4, True, 'Hello']
+mutable_list [0] = 5
+print (mutable_list)
