69c34a7484f6a31066b2c444514fed3d73bef2e1
GegerOk <166231334+GegerOk@users.noreply.github.com>
2024-05-06 11:12:43 +0300

    Update hw11.py

--- hw11.py
+++ hw11.py
@@ -2,10 +2,7 @@ def test():
     a = 20
     b = 30
     print(a, b)
-def test_2():
-    a = 21
-    b = 31
-    c = 41
+def test_2(a, b, c):
     print(a, b, c)
 test()
-test_2()
+test_2(21, 31, 41)
