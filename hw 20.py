a373a1acaf51eb035534b0924e61255241ae0b91
GegerOk <166231334+GegerOk@users.noreply.github.com>
2024-06-18 23:01:19 +0300

    Add files via upload

---
+++ hw 20.py
@@ -0,0 +1,11 @@
+class Building:
+    def __init__ (self, number):
+        print (Building)
+
+    def total_up (self):
+        for i in range (1, 41):
+            print (i)
+            self.number =+1
+
+building = Building (1)
+building.total_up()
