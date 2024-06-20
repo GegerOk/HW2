15b09e414baba5f7fdabdcf43f63b84d034412f8
GegerOk <166231334+GegerOk@users.noreply.github.com>
2024-06-20 19:27:52 +0300

    hw21.py

--- hw 21.py
+++ hw 21.py
@@ -1,1 +1,36 @@
-print ('hello world')
+class animal:
+    def __init__(self, name):
+        self.alive = True
+        self.Fed = False
+        self.name = name
+
+class plant:
+    def __init__(self, name, edible):
+        self.edible = edible
+        self.name = name
+
+class Mamal(animal):
+     def eat(self, food):
+        if food.edible == True:
+             print (f'{self.name} съел {food.name}')
+             self.Fed = True
+        else:
+            print (f"{self.name} не стал есть  {food.name}")
+            self.alive = False
+
+class Predator(animal):
+    pass
+
+class Flower(plant):
+    pass
+
+class Fruit(plant):
+    pass
+
+Cat = Mamal("Кот")
+apple = Fruit ("Яблоко", True)
+Krapiva = Flower("Крапива", False)
+Cat.eat (apple)
+print (Cat.alive)
+Cat.eat (Krapiva)
+print (Cat.alive)
