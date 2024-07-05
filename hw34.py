c2484836f4db9108d45321db38c3a8a07f6cd61b
GegerOk <166231334+GegerOk@users.noreply.github.com>
2024-07-05 17:52:11 +0300

    Add files via upload

---
+++ hw34.py
@@ -0,0 +1,18 @@
+def is_prime(func):
+    def wrapper( * args):
+        result = func( * args)
+        a = 3
+        while a != result:
+            a += 1
+            if result % a == 0:
+                return 'Составное'
+            else:
+                return 'Простое'
+    return wrapper
+
+@is_prime
+def sum_three(first, second, third):
+    return first + second + third
+
+result = sum_three(2, 4, 5)
+print(result)
