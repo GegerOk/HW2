6fcb78b0556cbcfd3f0f5e37b0fc13f2cfd63485
GegerOk <166231334+GegerOk@users.noreply.github.com>
2024-09-19 20:09:24 +0300

    Add files via upload

--- hw58.py
+++ hw58.py
@@ -1,25 +1,43 @@
-from fastapi import FastAPI, Path
-
+from fastapi import FastAPI, Path, HTTPException
+from pydantic import BaseModel
 app = FastAPI()
 
-users = {'1': 'Имя: Example, возраст 18'}
+global users
+users = []
+
+
+class User(BaseModel):
+    id: int
+    username: str 
+    age: int 
 
 @app.get ('/users')
-async def base() -> dict:
+async def base() -> list[User]:
     return users
 
 @app.post('/user/{username}/{age}')
-async def add_user(username, age) -> str:
-    curent_id= str(int(max(users, key=int)) +1)
-    users[curent_id] = f'Имя: {username}, возраст: {age}'
-    return f'User {curent_id} is registred'
+async def add_user(username, age) -> dict:
+    if users:
+        curent_id=(max(users.id for users in users) +1)
+    else:
+        curent_id = 1
+    user = User(id = curent_id, username=username, age=age)
+    users.append(user)
+    return user.dict()
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def edit_user(user_id, username, age) -> str:
-    users[user_id] = f'Имя: {username}, возраст: {age}'
-    return f'The user {user_id} is registred'
+async def edit_user(user_id, username, age) -> dict:
+    try:
+        user = User(id = user_id, username = username, age = age)
+        users[int(user_id) -1] = user
+    except IndexError:
+        raise HTTPException(status_code= 404, detail= 'User was not found')
+    return user.dict()
 
 @app.delete('/user/{user_id}')
-async def delete_user(user_id) -> str:
-    users.pop(user_id)
-    return 'User was deleted'
+async def delete_user(user_id) -> list[User]:
+    try:
+        users.pop(int(user_id)-1)
+    except IndexError:
+        raise HTTPException (status_code= 404, detail= 'User was not found')
+    return users
