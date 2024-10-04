351e8acb2c6dbb47fa6447866cd4ba1fa9b9cfde
GegerOk <166231334+GegerOk@users.noreply.github.com>
2024-10-04 10:57:33 +0300

    Add files via upload

---
+++ django_3/urban_django/task2/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+def base(request):
+    return render(request, 'second_task/base.html')
+    
+
+def func_temp(request):
+    return render(request, 'second_task/func_temp.html')
+
+def cats_page(request):
+    return render(request, 'second_task/cats_page.html')
+
+def trash_page(request):
+    return render(request, 'second_task/trash_page.html')
